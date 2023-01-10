3bdc841fe9586e4ce2ce8e50561f356691bf61c5
5ewon06 <gwonsewon03@gmail.com>
2023-01-11 08:38:46 +0900

    ADD : base setting

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../style/Home.scss';
+import PC from '../components/PC.js';
+
+const Home = () => {
+    const [isConn, setIsConn] = useState();
+
+    return (
+        <div>
+            <div className="home-wrap">
+                <div className="home-title-box">
+                    d
+                </div>
+                <Link to={'/page1'}>page1</Link>
+                <PC />
+            </div>
+        </div>
+    );
+}
+
+export default Home;
