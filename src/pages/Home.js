58084167c248bde10fd676820743a70fe12cbff8
sebanimm <102154824+sebanimm@users.noreply.github.com>
2023-01-11 13:55:09 +0900

    setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,21 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../style/Home.scss';
-import PC from '../components/PC.js';
-import Header from '../components/Header.js';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../style/Home.scss";
+import PC from "../components/PC.js";
+import Header from "../components/Header.js";
 
 const Home = () => {
-    const [isConn, setIsConn] = useState();
+	const [isConn, setIsConn] = useState();
 
-    return (
-        <div>
-            <div className="home-wrap">
-                <Header />
-                <div className="home-title-box">
-                    ds
-                </div>
-                <PC />
-            </div>
-        </div>
-    );
-}
+	return (
+		<div>
+			<div className="home-wrap">
+				<Header />
+				<div className="home-title-box">ds</div>
+				<PC />
+			</div>
+		</div>
+	);
+};
 
 export default Home;
--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import Header from '../components/Header.js';
+import { React, useEffect } from "react";
+import axios from "axios";
 
 const Page1 = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
-}
+	/** 날씨 API 가져오는 함수 하하 */
+	const getWeatherAPI = () => {
+		/** 인증키 */
+		const API_KEY = "1af9314497443ac739cced077296c089";
+		/** 위도 */
+		const lat = 35.18;
+		/** 적도 */
+		const lon = 129.0752;
+		const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+		axios.get(URL).then((res) => {
+			const data = res.data;
+			const weatherData = data.weather[0];
+			const mainData = data.main;
+			const windData = data.wind;
+			/** 기온(섭씨) */
+			const celTemp = parseInt(mainData.temp - 273.15);
+			/** 습도 */
+			const humidity = mainData.humidity;
+			/** 날씨 */
+			const weather = weatherData.main;
+			/** 풍속 */
+			const wind = Math.round(windData.speed);
+		});
+	};
 
-export default Page1; 
+	useEffect(() => {
+		getWeatherAPI();
+	});
+
+	return <div></div>;
+};
+
+export default Page1;
